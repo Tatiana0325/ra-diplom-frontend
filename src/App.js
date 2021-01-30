a5b79fd6ea968c47ebc89827f942ff9e1342af01
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2021-01-30 19:20:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import PageHome from './components/PageHome';
+import PageCatalog from './components/PageCatalog';
+import PageAbout from './components/PageAbout';
+import PageContacts from './components/PageContacts';
+import Page404 from './components/Page404';
+import PageProduct from './components/PageProduct';
+import PageCart from './components/PageCart';
+// eslint-disable-next-line
+// import regeneratorRuntime from 'regenerator-runtime';
 import './App.css';
+require.context('./img');
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Header />
+        <main className="container">
+          <div className="row">
+            <div className="col">
+              <Banner />
+              <Switch>
+                <Route exact path={`/catalog/:id`} component={PageProduct} />
+                <Route exact path={`/catalog`} component={PageCatalog} />
+                <Route exact path={`/about`} component={PageAbout} />
+                <Route exact path={`/contacts`} component={PageContacts} />
+                <Route exact path={`/cart`} component={PageCart} />
+                <Route exact path={`/`} component={PageHome} />
+                <Route path='*' component={Page404} />
+              </Switch>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </Router>
+    </React.Fragment>
   );
 }
-
-export default App;
